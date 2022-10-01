b6f0888ffeb1c1202ac47ec2bfe3968c956f9178
To Thanh Sang <sang.to@coinhako.com>
2022-10-02 02:12:58 +0700

    Config vercel

--- main.py
+++ main.py
@@ -128,6 +128,6 @@ def create_post(file: UploadFile = File(...), db: Session = Depends(get_db)):
     url = result.get("url")
     print("url: ", url)
 
-if __name__ == "__main__":
-    # uvicorn.run("main:app", port=9000, reload=True)
-     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     # uvicorn.run("main:app", port=9000, reload=True)
+#      uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
